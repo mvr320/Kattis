41cce5e14f3057b1a6fe432dc8ca37d18a348adc
MCJ Vasseur <michael@it.vu.nl>
2019-11-22 23:43:52 +0100

    Example solution for C++

---
+++ C++/aaah/solution.cpp
@@ -0,0 +1,19 @@
+// extract to string
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  std::string ah1, ah2;
+
+  std::getline (std::cin,ah1);
+  std::getline (std::cin,ah2);
+  
+  if(ah1.length()<ah2.length()){
+    std::cout << "no";
+  } else {
+    std::cout << "go";
+  }
+
+  return 0;
+}

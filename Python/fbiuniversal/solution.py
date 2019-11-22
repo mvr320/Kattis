dd604047000d8a7baf3e5cbdd35e608e65462507
MCJ Vasseur <michael@it.vu.nl>
2019-11-22 23:20:04 +0100

    Almost fixed

--- Python/fbiuniversal/solution.py
+++ Python/fbiuniversal/solution.py
@@ -10,11 +10,22 @@ for line in data[1:]:
     for (ci,cj) in repl:
         inp = inp.replace(ci,cj)
     inp = inp.strip('\n')
-    # (2*D1 + 4*D2 + 5*D3 + 7*D4 + 8*D5 + 10*D6 + 11*D7 + 13*D8) mod 27
-
-    for char in list(inp):
+    multi = [2,4,5,7,8,10,11,13]
+    summer = 0
+    for i in range(8):
+        char = list(inp)[i]
+        val = 0
         if not char.isdigit():
-            print(char)
-            print(1+string.ascii_uppercase.index(char))
-    #print(string.ascii_uppercase)
-    #string.ascii_lowercase
+            val = 10+string.ascii_uppercase.index(char)
+        else:
+            val = int(char)
+        summer += multi[i]*val
+    check = 0
+    char = list(inp)[8]
+    val = 0
+    if not char.isdigit():
+        val = 10+string.ascii_uppercase.index(char)
+    else:
+        val = int(char)
+    total = summer%27
+    print(ident, total, val)

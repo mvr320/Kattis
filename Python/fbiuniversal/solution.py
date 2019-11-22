e3fbae409de7486268a5ed0e09ad523afa1e459a
MCJ Vasseur <michael@it.vu.nl>
2019-11-22 22:58:12 +0100

    Implemented counting

--- Python/fbiuniversal/solution.py
+++ Python/fbiuniversal/solution.py
@@ -9,7 +9,12 @@ for line in data[1:]:
     repl = [('B','8'), ('G','C'), ('I','1'), ('O','0'), ('Q','0'), ('S','5'), ('U','V'), ('Y','V'), ('Z','2')]
     for (ci,cj) in repl:
         inp = inp.replace(ci,cj)
-    print(inp)
-    string.ascii_lowercase
-#
-    #'B','8'print(line)
+    inp = inp.strip('\n')
+    # (2*D1 + 4*D2 + 5*D3 + 7*D4 + 8*D5 + 10*D6 + 11*D7 + 13*D8) mod 27
+
+    for char in list(inp):
+        if not char.isdigit():
+            print(char)
+            print(1+string.ascii_uppercase.index(char))
+    #print(string.ascii_uppercase)
+    #string.ascii_lowercase

e8d05d8bf4e960bbaab2ac9b6a8d0d732bd5111e
MCJ Vasseur <michael@it.vu.nl>
2019-11-30 12:56:09 +0100

    First page done

--- Python/beavergnaw/beavergnaw.py
+++ Python/beavergnaw/beavergnaw.py
@@ -3,24 +3,11 @@ import itertools
 import math
 import string
 
-stor = {}
-wa = 0
-wb = 0
-total_votes = 0
-num_lines, num_region = [int(x) for x in input().split()]
-for nre in range(num_region):
-    stor[nre+1] = {'A':0, 'B':0}
-for _ in range(num_lines):
-    rid, va, vb = [int(x) for x in input().split()]
-    stor[rid]['A'] += va
-    stor[rid]['B'] += vb
-    total_votes += (va+vb)
-for nre in range(num_region):
-    total = stor[nre+1]['A']+stor[nre+1]['B']
-    winner = 'A' if stor[nre+1]['A']>stor[nre+1]['B'] else 'B'
-    exA = stor[nre+1]['A'] if winner=='B' else (stor[nre+1]['A']-1)-(total//2)
-    exB = stor[nre+1]['B'] if winner=='A' else (stor[nre+1]['B']-1)-(total//2)
-    wa += exA
-    wb += exB
-    print("{} {} {}".format(winner, exA, exB))
-print(abs(wa-wb)/total_votes)
+data = sys.stdin.readlines()[:-1]
+for line in data:
+    bd,v = [int(x) for x in line.split()]
+    c=bd/(2.0)*math.pi*(bd/2.0)**2
+    vq = c-v
+    d = bd*(vq/c)
+    #d = (vq*4.0*math.pi)**(1.0/3.0)
+    print(d)
--- Python/stararrangements/stararrangements.py
+++ Python/stararrangements/stararrangements.py
@@ -3,5 +3,11 @@ import itertools
 import math
 import string
 
-for val in [float(x) for x in sys.stdin.readlines()[1:]]:
-    print(math.ceil(val/400))
+num_stars = int(input())
+print("{}:".format(num_stars))
+for i in range(2,(1+(num_stars+1)//2) ):
+    for j in range(i-1,i+1):
+        if num_stars%(i+j)==0:
+            print("{},{}".format(i,j))
+        if num_stars%(i+j)==i:
+            print("{},{}".format(i,j))
